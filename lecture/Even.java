685783d1fef6bec2e70ec3a1119e5d15adfaa4d5
shumaila <49945353+18SW73@users.noreply.github.com>
2019-05-09 21:56:39 +0500

    Add files via upload

---
+++ lecture/Even.java
@@ -0,0 +1,22 @@
+class Even
+{
+	public static void main(String args[])
+	 {
+	    
+	    for(int i=0; i<10; i+=2)
+	     {
+             for(int j=0; j<10; j+=2)
+            {
+                  if(j==10)
+                  {
+                  	continue;
+                  }
+                  System.out.println(j);
+             }
+                   System.out.println("EVEN NUMBER'S");
+            
+              
+              
+	     }
+	 }
+}
---
+++ lecture/Odd.java
@@ -0,0 +1,21 @@
+class Odd
+{
+	public static void main(String args[])
+	 {
+	    
+	    
+             for(int j=1; j<10; j+=2)
+            {
+                  if(j==10)
+                  {
+                  	continue;
+                  }
+                  System.out.println(j);
+             }
+                   System.out.println("odd NUMBER'S");
+            
+              
+              
+	     
+	 }
+}
